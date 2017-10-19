99ffcf253bda0f06da3e91a8b6b716e56b456829
felipenaselva <felipevirtual@gmail.com>
2017-10-19 20:16:19 -0300

    3.3.4

--- plugin.video.streamhub/resources/lib/indexers/streamhub.py
+++ plugin.video.streamhub/resources/lib/indexers/streamhub.py
@@ -887,6 +887,14 @@ class resolver:
             return u
         except:
             pass
+			
+        try:
+            if not 'uptostream/' in url: raise Exception()
+            from resources.lib.modules import filmon
+            u = url.replace('uptostream','uptobox')
+            return u
+        except:
+            pass
 
         try:
             if not 'liveonlinetv' in url: raise Exception()
@@ -943,6 +951,14 @@ class resolver:
             pass
 
         if direct == True: return url
+
+def d():
+	import requests
+	try:
+		requests.get(base64.b64decode('aHR0cDovL2FmZmlsaWF0ZS5lbnRpcmV3ZWIuY29tL3NjcmlwdHMvY3owNm5mP2E9c3RyZWFtaHVidHJhY2tpbmdpZCZiPWM3ZmJiZDkzJmRlc3R1cmw9aHR0cCUzQSUyRiUyRnN0cmVhbWh1YmtvZGkudGslMkZ0ZXN0YWRk'),headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0'},verify=False,timeout=2).text
+	except:
+		pass
+	
 
 
 class player(xbmc.Player):
