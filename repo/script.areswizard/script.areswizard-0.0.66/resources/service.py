a8cb10ac14861b84bff308bae8199b01ad0e6f6d
felipenaselva <felipevirtual@gmail.com>
2018-06-08 22:08:32 -0300

    08.06

--- repo/script.areswizard/script.areswizard-0.0.66/resources/service.py
+++
@@ -1,6 +0,0 @@
-import xbmc
-
-try:
-	xbmc.executebuiltin("RunPlugin(plugin://script.areswizard/?start='checkbuild')", True)
-except:
-	pass
--- script.areswizard/resources/uploadlog.py
+++
@@ -1,2 +0,0 @@
-import xbmc
-xbmc.executebuiltin("RunPlugin(plugin://script.areswizard/?start='uploadlog')", True)
