1075446118ed775db39470948d35aaba00cc0389
felipenaselva <felipevirtual@gmail.com>
2018-06-18 23:24:46 -0300

    build tools

---
+++ plugin.program.Build.Tools/uservar.py
@@ -0,0 +1,114 @@
+import os, xbmc, xbmcaddon
+
+#########################################################
+### User Edit Variables #################################
+#########################################################
+ADDON_ID       = xbmcaddon.Addon().getAddonInfo('id')
+ADDONTITLE     = 'Build Tools'
+EXCLUDES       = [ADDON_ID, 'Kodish.repo.store']
+# Text File with build info in it.
+BUILDFILE      = 'https://pastebin.com/raw/awxe3cGQ'
+# How often you would list it to check for build updates in days
+# 0 being every startup of kodi
+UPDATECHECK    = 0
+# Text File with apk info in it.
+APKFILE        = 'https://kodi.tv/'
+# Text File with Youtube Videos urls.  Leave as 'http://' to ignore
+YOUTUBETITLE   = 'Videos Tutoriais'
+YOUTUBEFILE    = 'https://pastebin.com/raw/wfGAH8PG'
+# Text File for addon installer.  Leave as 'http://' to ignore
+ADDONFILE      = 'http://'
+# Text File for advanced settings.  Leave as 'http://' to ignore
+ADVANCEDFILE   = 'http://'
+
+# Dont need to edit just here for icons stored locally
+PATH           = xbmcaddon.Addon().getAddonInfo('path')
+ART            = os.path.join(PATH, 'resources', 'art')
+
+#########################################################
+### THEMING MENU ITEMS ##################################
+#########################################################
+# If you want to use locally stored icons the place them in the Resources/Art/
+# folder of the wizard then use os.path.join(ART, 'imagename.png')
+# do not place quotes around os.path.join
+# Example:  ICONMAINT     = os.path.join(ART, 'mainticon.png')
+#           ICONSETTINGS  = 'http://aftermathwizard.net/repo/wizard/settings.png'
+# Leave as http:// for default icon
+ICONBUILDS     = 'https://i.imgur.com/59BAHp8.png'
+ICONMAINT      = 'https://i.imgur.com/59BAHp8.png'
+ICONAPK        = 'https://i.imgur.com/59BAHp8.png'
+ICONADDONS     = 'https://i.imgur.com/59BAHp8.png'
+ICONYOUTUBE    = 'https://i.imgur.com/59BAHp8.png'
+ICONSAVE       = 'https://i.imgur.com/59BAHp8.png'
+ICONTRAKT      = 'https://i.imgur.com/59BAHp8.png'
+ICONREAL       = 'https://i.imgur.com/59BAHp8.png'
+ICONLOGIN      = 'https://i.imgur.com/59BAHp8.png'
+ICONCONTACT    = 'https://i.imgur.com/59BAHp8.png'
+ICONSETTINGS   = 'https://i.imgur.com/59BAHp8.png'
+# Hide the ====== seperators 'Yes' or 'No'
+HIDESPACERS    = 'Yes'
+# Character used in seperator
+SPACER         = '//'
+
+# You can edit these however you want, just make sure that you have a %s in each of the
+# THEME's so it grabs the text from the menu item
+COLOR1         = 'yellow'
+COLOR2         = 'white'
+# Primary menu items   / %s is the menu item and is required
+THEME1         = '[COLOR '+COLOR1+'][B][I]([COLOR '+COLOR2+']...[/COLOR])[/B][/COLOR] [COLOR '+COLOR2+']%s[/COLOR][/I]'
+# Build Names          / %s is the menu item and is required
+THEME2         = '[COLOR '+COLOR2+']%s[/COLOR]'
+# Alternate items      / %s is the menu item and is required
+THEME3         = '[COLOR '+COLOR1+']%s[/COLOR]'
+# Current Build Header / %s is the menu item and is required
+THEME4         = '[COLOR '+COLOR1+']Build Atual:[/COLOR] [COLOR '+COLOR2+']%s[/COLOR]'
+# Current Theme Header / %s is the menu item and is required
+THEME5         = '[COLOR '+COLOR1+']Thema Atual:[/COLOR] [COLOR '+COLOR2+']%s[/COLOR]'
+
+# Message for Contact Page
+# Enable 'Contact' menu item 'Yes' hide or 'No' dont hide
+HIDECONTACT    = 'Yes'
+# You can add \n to do line breaks
+CONTACT        = 'Obrigado por escolher o Vikings Build.Inscreva-se em nosso canal no Youtbe:  http://www.youtube.com/c/VikingsArcades'
+#Images used for the contact window.  http:// for default icon and fanart
+CONTACTICON    = 'http://'
+CONTACTFANART  = 'http://'
+#########################################################
+
+#########################################################
+### AUTO UPDATE #########################################
+########## FOR THOSE WITH NO REPO #######################
+# Enable Auto Update 'Yes' or 'No'
+AUTOUPDATE     = 'YES'
+# Url to wizard version
+WIZARDFILE     = 'https://pastebin.com/raw/awxe3cGQ'
+#########################################################
+
+#########################################################
+### AUTO INSTALL ########################################
+########## REPO IF NOT INSTALLED ########################
+# Enable Auto Install 'Yes' or 'No'
+AUTOINSTALL    = 'YES'
+# Addon ID for the repository
+REPOID         = 'Kodish.repo.store'
+# Url to Addons.xml file in your repo folder(this is so we can get the latest version)
+REPOADDONXML   = ''
+# Url to folder zip is located in
+REPOZIPURL     = 'http://sokodi.net/repo/Kodish.repo.store.zip'
+#########################################################
+
+#########################################################
+### NOTIFICATION WINDOW##################################
+#########################################################
+# Enable Notification screen Yes or No
+ENABLE         = 'Yes'
+# Url to notification file
+NOTIFICATION   = ''
+# Use either 'Text' or 'Image'
+HEADERTYPE     = ''
+HEADERMESSAGE  = ''
+# url to image if using Image 424x180
+HEADERIMAGE    = ''
+# Background for Notification Window
+BACKGROUND     = ''
+#########################################################
